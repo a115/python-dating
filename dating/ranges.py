6c121e4bc16816bfcf982b76b244cf171c662b48
Jordan Dimov <jordan.dimov@sky.uk>
2017-09-28 23:19:52 +0100

    DateTimeRange is more fundamental than DateRange

--- dating/ranges.py
+++ dating/ranges.py
@@ -1,13 +1,92 @@
-from datetime import date, timedelta
+from datetime import date, datetime, time, timedelta
 import iso8601
+import pytz
 
-class DateRange:
-    start_date = None
-    end_date = None
+class DateTimeRange:
+    def __init__(self, start_datetime=None, end_datetime=None, timezone=None):
+        """ Initialise a new DateTimeRange from the given start and end datetime objects. 
+        Ensure that the start and end datetimes are timezone-aware (use UTC by default). 
+        If timezone is passed, localise start and end datetimes to that zone. """
 
-    def __init__(self, start_date=None, end_date=None):
-        self.start_date = start_date or date.min
-        self.end_date = end_date or date.max
+        self._start_datetime = start_datetime or datetime.min
+        self._end_datetime = end_datetime or datetime.max
+        self.timezone = timezone or self._start_datetime.tzinfo or self._end_datetime.tzinfo or pytz.utc
+        if self.timezone != self._start_datetime.tzinfo:
+            self._start_datetime = self.timezone.localize(self._start_datetime)
+        if self.timezone != self._end_datetime.tzinfo:
+            self._end_datetime = self.timezone.localize(self._end_datetime)
+
+    @classmethod
+    def from_strings(cls, start_datetime_str=None, end_datetime_str=None):
+        return cls(start_datetime=iso8601.parse_date(start_datetime_str) if start_datetime_str else None,
+                   end_datetime=iso8601.parse_date(end_datetime_str) if end_datetime_str else None)
+
+    @property
+    def start_datetime(self):
+        return self._start_datetime
+
+    @property
+    def end_datetime(self):
+        return self._end_datetime
+
+    @property
+    def start_date(self):
+        return self._start_datetime.date()
+
+    @property
+    def end_date(self):
+        return self._end_datetime.date()
+
+    def __str__(self):
+        return "({} - {})".format(self._start_datetime, self._end_datetime)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __iter__(self):
+        self._pointer = self._start_datetime
+        self._step = timedelta(days=1)
+        return self
+
+    def is_well_defined(self):
+        return ((self._start_datetime > self.timezone.localize(datetime.min)) and 
+                (self._end_datetime < self.timezone.localize(datetime.max)))
+
+    def __contains__(self, a_datetime):
+        return a_datetime >= self._start_datetime and a_datetime <= self._end_datetime
+
+    def __next__(self):
+        if self._pointer <= self._end_datetime:
+            return_value = self._pointer
+            self._pointer += self._step
+            return return_value
+        raise StopIteration()
+
+    def __eq__(self, other):
+        return ((self._start_datetime == other.start_datetime) and
+                (self._end_datetime == other.end_datetime))
+
+    def __lt__(self, other):
+        return ((self._start_datetime < other.start_datetime) or 
+                ((self._start_datetime == other.start_datetime) and (self._end_datetime < other.end_datetime)))
+
+    def __le__(self, other):
+        return (self._start_datetime <= other.start_datetime) and (self._end_datetime <= other.end_datetime)
+
+    def __gt__(self, other):
+        return ((self._start_datetime > other.start_datetime) or 
+                (self._start_datetime == other.start_datetime) and (self._end_datetime > other.end_datetime))
+
+    def __ge__(self, other):
+        return (self._start_datetime >= other.start_datetime) and (self._end_datetime >= other.end_datetime)
+
+
+class DateRange(DateTimeRange):
+
+    def __init__(self, start_date=None, end_date=None, timezone=None):
+        start_datetime = datetime.combine(start_date or date.min, time.min)
+        end_datetime = datetime.combine(end_date or date.max, time.max)
+        super().__init__(start_datetime=start_datetime, end_datetime=end_datetime, timezone=timezone)
 
     @classmethod
     def from_strings(cls, start_date_str=None, end_date_str=None):
@@ -17,44 +96,6 @@ class DateRange:
     def __str__(self):
         return "({} - {})".format(self.start_date, self.end_date)
 
-    def __iter__(self):
-        self._pointer = self.start_date
-        self._step = timedelta(days=1)
-        return self
-
-    def is_well_defined(self):
-        return (self.start_date > date.min) and (self.end_date < date.max)
-
     def __contains__(self, a_date):
         return a_date >= self.start_date and a_date <= self.end_date
 
-    def __next__(self):
-        if self._pointer <= self.end_date:
-            return_value = self._pointer
-            self._pointer += self._step
-            return return_value
-        raise StopIteration()
-
-    def __eq__(self, other):
-        return ((self.start_date == other.start_date) and
-                (self.end_date == other.end_date))
-
-    def __lt__(self, other):
-        return self.start_date < other.start_date
-
-    def __le__(self, other):
-        return (self.start_date <= other.start_date) and (self.end_date <= other.end_date)
-
-    def __gt__(self, other):
-        return self.start_date > other.start_date
-
-    def __ge__(self, other):
-        return (self.start_date >= other.start_date) and (self.end_date >= other.end_date)
-
-
-class DateTimeRange(DateRange):
-
-    @classmethod
-    def from_strings(cls, start_date_str=None, end_date_str=None):
-        return cls(start_date=iso8601.parse_date(start_date_str) if start_date_str else None,
-                   end_date=iso8601.parse_date(end_date_str) if end_date_str else None)
--- tests/test_dating_ranges.py
+++ tests/test_dating_ranges.py
@@ -1,15 +1,49 @@
 import unittest
 import pytz
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, time
 from itertools import islice
+from random import shuffle
 
 from dating.ranges import DateRange, DateTimeRange
+
 
 TODAY = date.today()
 P1D = timedelta(days=1)
 P15D = timedelta(days=15)
 YESTERDAY = TODAY - P1D
 TOMORROW = TODAY + P1D
+
+DT_NOW = datetime.now()
+DT_TOMORROW = datetime.combine(TOMORROW, DT_NOW.time())
+DT_YESTERDAY = datetime.combine(YESTERDAY, DT_NOW.time())
+
+UTC = pytz.utc
+
+class TestDateTimeRange(unittest.TestCase):
+    def setUp(self):
+        self.start_datetime = DT_NOW
+        self.end_datetime = DT_NOW + P15D
+        self.before_start = DT_YESTERDAY
+        self.after_end = self.end_datetime + P1D
+        self.dtr = DateTimeRange(self.start_datetime, self.end_datetime)
+        self.left_open_dtr = DateTimeRange(None, self.end_datetime)
+        self.right_open_dtr = DateTimeRange(self.start_datetime, None)
+
+    def test_str(self):
+        dtr_str = str(self.dtr)
+        expected_str = "({} - {})".format(UTC.localize(self.start_datetime), 
+                                          UTC.localize(self.end_datetime))
+        self.assertEqual(dtr_str, expected_str)
+
+    def test_date_properties(self):
+        self.assertEqual(self.dtr.start_date, self.start_datetime.date())
+        self.assertEqual(self.dtr.end_date, self.end_datetime.date())
+
+    def test_string_constructor(self):
+        dtr1 = DateTimeRange.from_strings("2017-01-12T14:25:10", "2017-02-15T07:00:01")
+        self.assertEqual(dtr1.start_datetime, datetime(2017, 1, 12, 14, 25, 10, 0, UTC))
+        self.assertEqual(dtr1.end_datetime, datetime(2017, 2, 15, 7, 0, 1, 0, UTC))
+
 
 class TestDateRange(unittest.TestCase):
     def setUp(self):
@@ -54,7 +88,7 @@ class TestDateRange(unittest.TestCase):
         self.assertFalse(self.right_open_dr.is_well_defined())
 
     def test_iterator(self):
-        expected_dates = [self.start_date + timedelta(days=x) for x in range(16)]
+        expected_dates = [UTC.localize(datetime.combine(self.start_date, time.min) + timedelta(days=x)) for x in range(16)]
         self.assertEqual(list(self.dr), expected_dates)
         self.assertEqual(list(islice(self.right_open_dr, 16)), expected_dates)
 
@@ -65,10 +99,14 @@ class TestDateRange(unittest.TestCase):
     def test_gt(self):
         dr2 = DateRange(YESTERDAY)
         self.assertTrue(self.dr > dr2)
+        dr3 = DateRange(TODAY)
+        self.assertTrue(dr3 > self.dr)
 
     def test_lt(self):
         dr2 = DateRange(YESTERDAY)
         self.assertTrue(dr2 < self.dr)
+        dr3 = DateRange(TODAY)
+        self.assertTrue(self.dr < dr3)
     
     def test_gte(self):
         dr2 = DateRange(TODAY, TODAY+timedelta(days=12))
@@ -78,27 +116,9 @@ class TestDateRange(unittest.TestCase):
         dr2 = DateRange(TOMORROW, TODAY+P15D)
         self.assertTrue(self.dr <= dr2)
 
-
-DT_NOW = datetime.now()
-DT_TOMORROW = datetime.combine(TOMORROW, DT_NOW.time())
-DT_YESTERDAY = datetime.combine(YESTERDAY, DT_NOW.time())
-
-class TestDateTimeRange(unittest.TestCase):
-    def setUp(self):
-        self.start_date = DT_NOW
-        self.end_date = DT_NOW + P15D
-        self.before_start = DT_YESTERDAY
-        self.after_end = self.end_date + P1D
-        self.dtr = DateTimeRange(self.start_date, self.end_date)
-        self.left_open_dtr = DateTimeRange(None, self.end_date)
-        self.right_open_dtr = DateTimeRange(self.start_date, None)
-
-    def test_str(self):
-        dtr_str = str(self.dtr)
-        expected_str = "({} - {})".format(self.start_date, self.end_date)
-        self.assertEqual(dtr_str, expected_str)
-
-    def test_string_constructor(self):
-        dtr1 = DateTimeRange.from_strings("2017-01-12T14:25:10", "2017-02-15T07:00:01")
-        self.assertEqual(dtr1.start_date, datetime(2017, 1, 12, 14, 25, 10, 0, pytz.utc))
-        self.assertEqual(dtr1.end_date, datetime(2017, 2, 15, 7, 0, 1, 0, pytz.utc))
+    def test_sort(self):
+        dr1 = DateRange(YESTERDAY)
+        dr2 = DateRange(TOMORROW, TODAY+P15D)
+        ranges = [self.dr, dr2, dr1, self.left_open_dr, self.right_open_dr]
+        shuffle(ranges)
+        self.assertEqual(sorted(ranges), [self.left_open_dr, dr1, self.dr, self.right_open_dr, dr2])
